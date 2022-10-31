cb9529059df64616ab44b0715cb2db4c7a7f7cd1
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2022-10-31 11:27:52 -0400

    Update pickem.py

--- pickem.py
+++ pickem.py
@@ -1,11 +1,13 @@
 teamNames = {'ari': "cardinals", 'atl' : "falcons", 'bal' : "ravens", 'buf':"bills", 'car':"panthers", 
 'chi':"bears", 'cin':"bengals", 'cle':"browns", 'dal':"cowboys",'den':"broncos", 'det':"lions", 
-'gb':"packers", 'hou':"texans", 'ind':"colts", 'jax':"jaguars", 'kc':"cheifs", 'lv':"raiders", 
+'gb':"packers", 'hou':"texans", 'ind':"colts", 'jax':"jaguars", 'kc':"chiefs", 'lv':"raiders", 
 'lar':'rams', 'lac':'chargers', 'mia':'dolphins', 'min':'vikings', 'ne':'patriots', 'no':'saints', 
 'nyg':'giants', 'nyj':'jets', 'phi':'eagles', 'pit':'steelers', 'sf':'49ers', 'sea':'seahawks'
 , 'tb':'buccaneers', 'ten':'titans', 'wsh':'commanders'}
 
-# parameters(text file with picks, text file with picks, text file with winners, boolea that speicifies format you want to output)
+# picksPool is the file where picks are made, recordsPool is output, winnersFileName is 
+# where winners from the week are, weekbyweek is a boolean
+# for how you want the output to look
 def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek):
     picks_f = open(picksPool, "r")
     winners_f = open(winnersFileName, "r")
@@ -34,14 +36,15 @@ def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek):
             if recordsHash.get(name) == None:
                 recordsHash[name] = [0, 0, 0]
 
-            for i in range (1, len(line)):
-                if line[i] in winners:
+            for team in winners:
+                if team in line:
                     wins += 1
+                elif team == "tie":
+                    ties += 1
                 else:
                     losses += 1
+            
             if "tie" in winners:
-                losses -= 1
-                ties += 1
                 recordsHash[name].append("Week " + str(week) + ": " + str(wins) + "-" + str(losses) + "-" + str(ties))
                 recordsHash[name][0] += wins
                 recordsHash[name][1] += losses
@@ -50,23 +53,41 @@ def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek):
                 recordsHash[name].append("Week " + str(week) + ": " + str(wins) + "-" + str(losses))
                 recordsHash[name][0] += wins
                 recordsHash[name][1] += losses
-            
+
+    # sorting
+    percentageHash = {}
+    for name in recordsHash:
+        percentageHash[name] = (recordsHash[name][0]/(recordsHash[name][0] +
+                recordsHash[name][1]))*100
+    sortedNames = []
+    sorted_keys = sorted(percentageHash, key=percentageHash.get, reverse=True)
+    for w in sorted_keys:
+        sortedNames.append(w)
+
     if weekByWeek:
-        for name in recordsHash:
+        for name in sortedNames:
             records_f.write(name + "\n")
-            records_f.write("Total: " + str(recordsHash[name][0]) + "-" + str(recordsHash[name][1]) + "-" + 
-            str(recordsHash[name][2]) + "\n")
+            records_f.write("Total: " + str(recordsHash[name][0]) + "-" + str(recordsHash[name][1]))
+            if recordsHash[name][2] == 0:
+                records_f.write(" (" + "{:.2f}".format(percentageHash[name]) + "%)\n")
+            else:
+                records_f.write("-" + str(recordsHash[name][2]) + " (" 
+                + "{:.2f}".format(percentageHash[name]) + "%)\n")
+
             for i in range(3, len(recordsHash[name])):
                 records_f.write(recordsHash[name][i] + '\n')
             records_f.write('\n')
     else:
-        for name in recordsHash:
-            records_f.write(name.capitalize() + "\n")
-            records_f.write("Total: " + str(recordsHash[name][0]) + "-" + str(recordsHash[name][1]) + "-" + 
-            str(recordsHash[name][2]) + "\n")
+        for name in sortedNames:
+            records_f.write(name + "\n")
+            records_f.write("Total: " + str(recordsHash[name][0]) + "-" + str(recordsHash[name][1]))
+            if recordsHash[name][2] == 0:
+                records_f.write(" (" + "{:.2f}".format(percentageHash[name]) + "%)\n")
+            else:
+                records_f.write("-" + str(recordsHash[name][2]) + " (" 
+                + "{:.2f}".format(percentageHash[name]) + "%)\n")
             records_f.write("Last week: " + (str(recordsHash[name][len(recordsHash[name]) - 1]).split())[2] + "\n\n")
 
-weekByWeek = True
 while True:
     weekByWeek = input("Every week?(y/n) ")
     if weekByWeek.lower() == "y":
