f365bfc83f368f0c25a3e5932148bb8d213c75e0
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2022-12-15 01:49:31 -0500

    Update pickem.py

--- pickem.py
+++ pickem.py
@@ -1,3 +1,21 @@
+import time
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import client, file, tools
+from bs4 import BeautifulSoup as bs
+import urllib.request
+
+SCOPES = "https://www.googleapis.com/auth/forms.responses.readonly"
+DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
+
+store = file.Storage('token.json')
+creds = None
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = discovery.build('forms', 'v1', http=creds.authorize(
+    Http()), discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False)
+
 teamNames = {'ari': "cardinals", 'atl' : "falcons", 'bal' : "ravens", 'buf':"bills", 'car':"panthers", 
 'chi':"bears", 'cin':"bengals", 'cle':"browns", 'dal':"cowboys",'den':"broncos", 'det':"lions", 
 'gb':"packers", 'hou':"texans", 'ind':"colts", 'jax':"jaguars", 'kc':"chiefs", 'lv':"raiders", 
@@ -5,10 +23,73 @@ teamNames = {'ari': "cardinals", 'atl' : "falcons", 'bal' : "ravens", 'buf':"bil
 'nyg':'giants', 'nyj':'jets', 'phi':'eagles', 'pit':'steelers', 'sf':'49ers', 'sea':'seahawks'
 , 'tb':'buccaneers', 'ten':'titans', 'wsh':'commanders'}
 
-# picksPool is the file where picks are made, recordsPool is output, winnersFileName is 
-# where winners from the week are, weekbyweek is a boolean
-# for how you want the output to look
-def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek):
+
+
+
+chats = {
+    
+}
+
+
+toBeAdded = {}
+
+def readForm(chatName, week):
+    fid = open('/Users/jonas/python-workspace/pickem files/form links.txt', 'r')
+    while fid.readline().lower().strip() != 'week ' + str(week): pass
+
+    while True:
+        line = fid.readline()
+        if chatName in line: 
+            id = line.split()[-1]
+            break
+
+    result = service.forms().responses().list(formId=id).execute()
+
+    ind = -1
+    for i in range(0, len(result['responses'])):
+        responseArr = []
+        for response in result['responses'][0]['answers']: 
+            responseArr.append(result['responses'][i]['answers'][response]['textAnswers']['answers'][0]['value'])
+
+        if ind == -1:
+            for r in responseArr:
+                if r in chats[chatName][1]:
+                    ind = responseArr.index(r)
+            if ind == -1: continue
+            
+        if responseArr[ind] in toBeAdded.keys(): 
+            raise ImportError ("duplicate username found in chat " + str(chatName))
+
+        toBeAdded[responseArr[ind]] = ""
+
+        for r in responseArr:
+            if r.lower() in teamNames.values():
+                toBeAdded[responseArr[ind]] +=  ' ' + r
+
+def addWinners(week):
+    f = open('/Users/jonas/python-workspace/pickem files/winners.txt', 'a')
+    f.write("\n")
+    page = urllib.request.urlopen(f'https://www.espn.com/nfl/scoreboard/_/week/{week}/year/2022/seasontype/2')
+    soup = bs(page)
+    teams = soup.body.findAll('div', {"class": "ScoreCell__TeamName"})
+    scores = soup.body.findAll('div', {"class": "ScoreCell__Score"})
+
+    i = 0
+    games = [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},]
+
+    for team in teams:
+        games[int(i/2)][team.text] = int(scores[i].text)
+        i += 1
+
+    winners = set()
+    for game in games:
+        if game == {}: break
+        f.write((max(game, key = game.get)).lower() + " ")
+    f.close()
+
+
+
+def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek, chatName):
     picks_f = open(picksPool, "r")
     winners_f = open(winnersFileName, "r")
     records_f = open(recordsPool, "r+")
@@ -16,6 +97,7 @@ def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek):
     recordsHash = {}
     for line in picks_f:
         line = line.lower().split()
+        if len(line) == 0: continue
         if line[0] == "week":
             week = int(line[1])
         winners = ""
@@ -32,7 +114,7 @@ def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek):
                 line[0] += " " + line[1].capitalize()
                 line.pop(1)
 
-            name = line[0]
+            name = line[0].lower()
             if recordsHash.get(name) == None:
                 recordsHash[name] = [0, 0, 0]
 
@@ -63,10 +145,10 @@ def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek):
     sorted_keys = sorted(percentageHash, key=percentageHash.get, reverse=True)
     for w in sorted_keys:
         sortedNames.append(w)
-
+    records_f.write(f"@all {chatName} PICKEM\n\n")
     if weekByWeek:
         for name in sortedNames:
-            records_f.write(name + "\n")
+            records_f.write("@" + name + "\n")
             records_f.write("Total: " + str(recordsHash[name][0]) + "-" + str(recordsHash[name][1]))
             if recordsHash[name][2] == 0:
                 records_f.write(" (" + "{:.2f}".format(percentageHash[name]) + "%)\n")
@@ -79,7 +161,7 @@ def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek):
             records_f.write('\n')
     else:
         for name in sortedNames:
-            records_f.write(name + "\n")
+            records_f.write("@" + name + "\n")
             records_f.write("Total: " + str(recordsHash[name][0]) + "-" + str(recordsHash[name][1]))
             if recordsHash[name][2] == 0:
                 records_f.write(" (" + "{:.2f}".format(percentageHash[name]) + "%)\n")
@@ -87,11 +169,18 @@ def pickProcessing(picksPool, recordsPool, winnersFileName, weekByWeek):
                 records_f.write("-" + str(recordsHash[name][2]) + " (" 
                 + "{:.2f}".format(percentageHash[name]) + "%)\n")
             records_f.write("Last week: " + (str(recordsHash[name][len(recordsHash[name]) - 1]).split())[2] + "\n\n")
+    
 
-while True:
-    weekByWeek = input("Every week?(y/n) ")
-    if weekByWeek.lower() == "y":
-        break
-    elif weekByWeek.lower() == "n":
-        weekByWeek = False
-        break
+def processAllChats(weekByWeek, week):
+    for key in chats:
+        readForm(key, week)
+    for key in chats:
+        picks_f = open(chats[key][0], 'a')
+        picks_f.write(f"\nWeek {week}\n")
+        for name in toBeAdded:
+            if name in chats[key][1]:
+                picks_f.write(f'{name} {toBeAdded[name]}\n')
+        picks_f.close()
+    # for key in chats:
+    #     pickProcessing(chats[key][0], chats[key][2], winnersFileName, weekByWeek, key)
+
